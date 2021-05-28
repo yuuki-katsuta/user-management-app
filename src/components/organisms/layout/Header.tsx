0c6c9f34c1fa8215373ce77f698f8599f8d1736b
yuuki-katsuta <reptpr0314@gmail.com>
2021-05-29 08:59:44 +0900

    ルーティング実装

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,24 @@
-import { memo, VFC } from 'react';
+import { memo, useCallback, VFC } from 'react';
 import { Flex, Heading, Link, Box } from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
 import { MenuIconButton } from '../../atoms/button/MenuIconButton';
 import { MenuDrawer } from '../../molecules/MenuDrawer';
+import { useHistory } from 'react-router-dom';
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const history = useHistory();
+  const onClickHome = useCallback(() => history.push('/home'), [history]);
+  const onClickUserManagement = useCallback(
+    () => history.push('/home/user_management'),
+    [history]
+  );
+  const onClickSetting = useCallback(
+    () => history.push('/home/setting'),
+    [history]
+  );
+
   return (
     <>
       <Flex
@@ -16,7 +29,13 @@ export const Header: VFC = memo(() => {
         justify='space-between'
         padding={{ base: 3, md: 5 }}
       >
-        <Flex align='center' as='a' mr={8} _hover={{ cursor: 'pointer' }}>
+        <Flex
+          align='center'
+          as='a'
+          mr={8}
+          _hover={{ cursor: 'pointer' }}
+          onClick={onClickHome}
+        >
           <Heading as='h1' fontSize={{ base: 'md', md: 'lg' }}>
             ユーザー管理アプリ
           </Heading>
@@ -28,13 +47,19 @@ export const Header: VFC = memo(() => {
           display={{ base: 'none', md: 'flex' }}
         >
           <Box pr={4}>
-            <Link>ユーザー一覧</Link>
-            <Link>設定</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
+            <Link onClick={onClickSetting}>設定</Link>
           </Box>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer isOpen={isOpen} onClose={onClose} />
+      <MenuDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
