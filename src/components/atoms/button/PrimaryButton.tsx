821ef7bdea392035050e3c059bb6088ca7228bfd
yuuki-katsuta <reptpr0314@gmail.com>
2021-05-29 10:31:41 +0900

    ログイン画面のコンポーネント分割

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,15 @@
+import { memo, VFC, ReactNode } from 'react';
+import { Button } from '@chakra-ui/react';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const PrimaryButton: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <Button bg='teal.400' color='white' isFullWidth _hover={{ opacity: 0.8 }}>
+      {children}
+    </Button>
+  );
+});
